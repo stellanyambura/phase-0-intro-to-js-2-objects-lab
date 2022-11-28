81d91784826d14ca7189532f81861cc5db1afc4a
stellagitonga <stellanyamburagitonga@gmail.com>
2022-11-28 15:56:42 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Saviour",
+    streetAddress: "Drumvale"
+  }
+  
+  function  updateEmployeeWithKeyAndValue(){
+    return {...employee,["name"]: "Sam",["streetAddress"]: "11 Broadway"}
+  }
+   
+  function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name="Sam",
+    employee.streetAddress="12 Broadway"
+    return employee;
+  }
+  
+  
+  function deleteFromEmployeeByKey(employee,key){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+     return newEmployee;
+  }
+  deleteFromEmployeeByKey();
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name
+    return employee;
+  }
